6137c92a855cfc6a8c4088ce4e6f00e72e0d1036
barbaraeguche <barbaraeguche@icloud.com>
2026-05-02 20:36:27 -0400

    refactor

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import { Analytics } from "@vercel/analytics/react";
 import Decorations from "@/components/decorations";
 import Layout from "@/components/layout";
 import About from "@/sections/about";
 import Experience from "@/sections/experience";
 import Projects from "@/sections/projects";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
   return (
@@ -14,7 +14,7 @@ export default function App() {
         <Experience/>
         <Projects/>
       </Layout>
-      <Analytics />
+      <Analytics/>
     </>
   );
 }
